768e0fd53ab3e32bcbb1c5fe997d63ec72097cdb
Uncle Cheese <aaron@silverstripe.com>
2014-12-11 15:38:18 +1300

    Lesson 5: The holder/page pattern
    Lesson 5: The holder/page pattern
    
    remove default parent
    
    replace allowed children

---
+++ mysite/code/ArticleHolder.php
@@ -0,0 +1,12 @@
+<?php
+
+class ArticleHolder extends Page {
+
+	private static $allowed_children = array (
+		'ArticlePage'
+	);
+}
+
+class ArticleHolder_Controller extends Page_Controller {
+
+}
---
+++ mysite/code/ArticlePage.php
@@ -0,0 +1,11 @@
+<?php
+
+class ArticlePage extends Page {
+
+	private static $can_be_root = false;
+	
+}
+
+class ArticlePage_Controller extends Page_Controller {
+
+}
