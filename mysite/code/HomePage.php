4ee3d0cbeb5f58efe31b37d81527dbf37f4ab6d9
Uncle Cheese <aaron@silverstripe.com>
2015-02-05 15:52:43 +1300

    Lesson 8 complete

--- mysite/code/HomePage.php
+++ mysite/code/HomePage.php
@@ -6,5 +6,9 @@ class HomePage extends Page {
 
 class HomePage_Controller extends Page_Controller {
 
-	
+	public function LatestArticles($count = 3) {
+		return ArticlePage::get()
+					->sort('Created', 'DESC')
+					->limit($count);
+	}	
 }
