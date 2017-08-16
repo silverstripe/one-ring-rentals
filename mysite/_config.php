0302c4ce86b6241652150204f9073cd943f9cdb8
Simon Gow <simon@silverstripe.com>
2017-08-17 06:52:09 +1000

    Upgrade one ring rentals to SilverStripe 4.0.0-beta2
    - SS_Log was replaced in new SS4 namespaces, replace it with an
    updated monolog config.

--- mysite/_config.php
+++ mysite/_config.php
@@ -13,14 +13,3 @@ require_once('conf/ConfigureFromEnv.php');
 
 // Set the site locale
 i18n::set_locale('en_US');
-
-if(Director::isTest()) {
-    SS_Log::add_writer(new SS_LogFileWriter('../silverstripe-errors-warnings.log'), SS_Log::WARN, '<=');
-
-    SS_Log::add_writer(new SS_LogFileWriter('../silverstripe-errors.log'), SS_Log::ERR);
-}
-
-if(Director::isLive()) {
-    SS_Log::add_writer(new SS_LogEmailWriter('me@example.com'), SS_Log::ERR);
-}
-
